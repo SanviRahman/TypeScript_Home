a5697ac99a22e1a92827b02d9689f3c05689f17c
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-05-31 23:47:31 +0600

    last

---
+++ Practice/array.js
@@ -0,0 +1,5 @@
+var userName;
+userName = ["Shanto", "Sanvi", "Pinky", 101];
+for (var i = 0; i < userName.length; i++) {
+    console.log(userName[i]);
+}
---
+++ Practice/array.ts
@@ -0,0 +1,7 @@
+let userName:(string|number)[];
+userName=["Shanto","Sanvi","Pinky",101];
+
+for(let i=0; i<userName.length;i++)
+{
+    console.log(userName[i]);
+}
