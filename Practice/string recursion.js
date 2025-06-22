cc01516c46879be5c858ae6e0d193a05edfd2a7e
Sanvi Rahman <121854279+SanviRahman@users.noreply.github.com>
2025-06-23 00:12:48 +0600

    late night

---
+++ Practice/string recursion.js
@@ -0,0 +1,21 @@
+function swap(str, i, j) {
+    var _a;
+    var char = str.split('');
+    _a = [char[j], char[i]], char[i] = _a[0], char[j] = _a[1]; //swap
+    return char.join('');
+}
+function perMutation(str, left, right) {
+    if (left == right) {
+        console.log(str);
+        return;
+    }
+    var i = left;
+    while (i <= right) {
+        str = swap(str, left, i);
+        perMutation(str, left + 1, right);
+        str = swap(str, left, i);
+        i++;
+    }
+}
+var s = "abc";
+perMutation(s, 0, s.length - 1);
